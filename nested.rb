0e3533dbcec34c24b49851750fd5e7d8582a0e02
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-11 03:53:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,11 +66,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = {
- 		    :yukihiro_matsumoto => {
- 		       :known_for => "Ruby",
-           :languages => ["LISP", "C"]
- 		    },
+	programmer_hash = 
+	
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,6 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
 
 end
 
